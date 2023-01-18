12ee414df5afd83c65382c7765bfbb268f382e64
Manav Misra <manavm@visionwebsoft.com>
2023-01-18 10:13:48 -0600

    feat: single student GET

--- app/index.js
+++ app/index.js
@@ -11,6 +11,20 @@ app.get("/api/students/attendance", (_, res) => {
   res.json(STUDENTS.map((student) => student.name));
 });
 
+// ! RECORDING
+app.get("/api/students/:id", (req, res) => {
+  // * This is a string
+  const { id } = req.params;
+
+  const student = STUDENTS.find((student) => student.id === Number(id));
+
+  if (student) {
+    res.json(student);
+  } else {
+    res.status(404).json({ message: "Student not found" });
+  }
+});
+
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
 });
