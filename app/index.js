91e9ce87ec409280ffce9978da1c3c5fcb33205f
Manav Misra <manavm@visionwebsoft.com>
2023-01-18 09:44:05 -0600

    feat: api/students

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-import GRADES from "./GRADES.js";
+import express from "express";
+import STUDENTS from "./GRADES.js";
+
+const app = express();
+
+app.get("/api/students", (_, res) => {
+  res.json(STUDENTS);
+});
+
+app.listen(3000, () => {
+  console.info("Server is running on port 3000");
+});
