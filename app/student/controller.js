cf8485438fd29379fa45fd35c0fb8a0ba17de06a
Manav Misra <manavm@visionwebsoft.com>
2023-01-27 11:42:39 -0600

    Set the possibly pointless 'strictQuery'

--- app/student/controller.js
+++ app/student/controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import config from "../config.js";
 import Student from "./Student.js";
 
-// Connect to the database
+mongoose.set("strictQuery", true);
 mongoose
   .connect(config.getDbConn("students"))
   .then(() => {
