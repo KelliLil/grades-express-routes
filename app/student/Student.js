85901203999c59965ccfce059af08b909e884d62
Manav Misra <manavm@visionwebsoft.com>
2023-01-30 13:29:20 -0600

    Add validation for unique grade names in Student

--- app/student/Student.js
+++ app/student/Student.js
@@ -1,18 +1,41 @@
 import { Schema, model } from "mongoose";
-import GradeSchema from "./grade-schema.js";
+import gradeSchema from "./grade-schema";
 
-const StudentSchema = new Schema({
-  name: { type: String, required: [true, "Student name is required"] },
-  minLength: [3, "Student name must be at least 3 characters long"],
-  trim: true,
-  validate: {
-    validator(name) {
-      // Only allow letters and spaces (one space in between words)
-      return /[a-zA-Z]+([\s][a-zA-Z]+)*/.test(name);
+const StudentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Student name is required"],
+      minLength: [3, "Student name must be at least 3 characters long"],
+      trim: true,
+      validate: {
+        validator(name) {
+          // Only allow letters and spaces (one space in between words)
+          return /[a-zA-Z]+([\s][a-zA-Z]+)*/.test(name);
+        },
+        message:
+          "Student name must only contain letters and only one space in between names",
+      },
     },
-    message:
-      "Student name must only contain letters and only one space in between names",
+
+    // Specify an array of subdocuments. Each subdocument will be a gradeSchema.
+    grades: [gradeSchema],
   },
-});
+  {
+    strict: "throw",
+
+    // Don't include the __v field in the document
+    versionKey: false,
+  }
+);
+
+// Validation to prevent duplicate grade names
+// Child schema validation must be done in the parent schema
+StudentSchema.path("grades").validate((grades) => {
+  const gradeNames = grades.map((grade) => grade.name.toLowerCase());
+
+  // If the number of unique grade names is less than the number of grades, then there are duplicates
+  return new Set(gradeNames).size === gradeNames.length;
+}, "Grade names must be unique");
 
 export default model("Student", StudentSchema);
