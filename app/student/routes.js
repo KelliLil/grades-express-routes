b2283c64f258d4cf2080daa135bab59742984e8e
Manav Misra <manavm@visionwebsoft.com>
2023-02-01 10:46:40 -0600

    feat: :sparkles: router.get("/:id 🧑‍🎓

--- app/student/routes.js
+++ app/student/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import mongoose from "mongoose";
 import studentController from "./controller.js";
 
 const router = new Router();
@@ -14,6 +15,24 @@ router.get("/", (_, res) => {
     .catch((err) => {
       res.status(500).json({ message: err.message });
     });
+});
+
+router.get("/:id", async (req, res) => {
+  const foundStudent = await studentController
+    .getStudentById(req.params.id)
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError && err.kind === "ObjectId") {
+        res.status(400).json({ message: "Invalid ID" });
+      } else {
+        res.status(500).json({ message: err.message });
+      }
+    });
+
+  if (foundStudent) {
+    res.json(foundStudent);
+  } else {
+    res.status(404).json({ message: "Student not found" });
+  }
 });
 
 export default router;
