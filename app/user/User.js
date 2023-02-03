ee2387dc1a05c9e6523f014182eefbcbc24bb7e3
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-03 12:42:17 -0600

    Remove logs

--- app/user/User.js
+++ app/user/User.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { Schema, model } from "mongoose";
+import { model, Schema } from "mongoose";
 import config from "../config.js";
 
 const UserSchema = new Schema({
@@ -25,10 +25,8 @@ const UserSchema = new Schema({
 });
 
 UserSchema.pre("save", async function (next) {
-  console.log("PRE SAVE");
   // * Only hash the password if it has been modified (or is new)
   if (this.isModified("password")) {
-    console.log(this.password, "PASSWORD");
     const generatedSalt = await bcrypt.genSalt(config.saltRounds);
     this.password = await bcrypt.hash(this.password, generatedSalt);
   }
