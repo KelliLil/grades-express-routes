d6d15ab6dfdbf66a08ff687c792d5820ab6212c9
Manav Misra <manavm@visionwebsoft.com>
2023-02-03 10:27:22 -0600

    refactor: :building_construction: initServer

--- app/index.js
+++ app/index.js
@@ -1,21 +1,3 @@
-import express from "express";
-import config from "./config.js";
-import studentRoutes from "./student/routes.js";
+import initServer from "./server.js";
 
-const app = express();
-
-// Tell express to parse the request body as JSON
-// Without this, req.body will be undefined
-// * THIS MIDDLEWARE MUST BE BEFORE THE ROUTES
-app.use(express.json());
-
-// Any requests to /api/students will be handled by studentRoutes
-app.use("/api/students/", studentRoutes);
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
-app.listen(config.port, () => {
-  console.info(`Server running on: http://localhost:${config.port}`);
-});
+initServer();
---
+++ app/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import config from "./config.js";
+import decodeUser from "./middleware/decode-user.js";
+import studentRoutes from "./student/routes.js";
+
+export default () => {
+  const app = express();
+
+  // * Middleware order matters!
+  app.use(express.json());
+  app.use(decodeUser);
+  app.use("/api/students/", studentRoutes);
+
+  app.use((_, res) => {
+    res.status(404).json({ message: "Not found" });
+  });
+
+  app.listen(config.port, () => {
+    console.info(`Server running on: http://localhost:${config.port}`);
+  });
+};
